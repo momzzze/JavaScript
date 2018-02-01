6bad043c6a7f2c12a07cf7d54d252ae031e840ec
momzzze <momzzze@abv.bg>
2018-02-01 19:47:17 +0200

    fixed NonDec problem

--- fundamentals/ArraysAndMatrices/NonDecreasingSubsequence/app.js
+++ fundamentals/ArraysAndMatrices/NonDecreasingSubsequence/app.js
@@ -6,8 +6,8 @@ function main(arr) {
     result.push(arr[0]);
 
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i]>bigestNumber) {
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i]>=bigestNumber) {
             bigestNumber = arr[i];
             result.push(arr[i]);
         }
