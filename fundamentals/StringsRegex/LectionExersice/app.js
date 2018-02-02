6b644a8cad2504776d99814e996f31c5d9c85968
momzzze <momzzze@abv.bg>
2018-02-02 18:31:47 +0200

    Strings

---
+++ fundamentals/StringsRegex/LectionExersice/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+//function letters(str) {
+   // for (let i = 0; i < str.length; i++) {
+     //   console.log(`str[${i}] -> ${str[i]}`)
+  //  }
+//}
+//letters("SoftUni");
+
+
+
+//function reverseStrings(str) {
+   // console.log(str.join('').split('').reverse().join(''))
+//}
+
+
+//reverseStrings(["SoftUni","Pesho","Gosho"])
+let str="JS JS JS"
+console.log(str.replace(/JS/g,"C#"));
+
