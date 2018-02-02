05f9d79e249827eec036397682c9620f39373622
momzzze <momzzze@abv.bg>
2018-02-02 19:24:07 +0200

    s

--- fundamentals/StringsRegex/LectionExersice/app.js
+++ fundamentals/StringsRegex/LectionExersice/app.js
@@ -15,6 +15,37 @@
 
 
 //reverseStrings(["SoftUni","Pesho","Gosho"])
-let str="JS JS JS"
-console.log(str.replace(/JS/g,"C#"));
+//let str="JS JS JS"
+//console.log(str.replace(/JS/g,"C#"));
 
+//function countOccurrences(target,str) {
+  // let counter=0
+   //while (true) {
+    //   let startIndex=str.indexOf(target)
+    //   if (startIndex<0) {
+    //       break
+    //   }
+    //   counter++
+     //  str=str.substr(startIndex+1)
+   //}
+//   console.log(counter)
+//}
+
+
+function extractText(str) {
+    let result=[]
+    
+    while (true) {
+        let start=str.indexOf('(')
+        if (start<0) {
+            break;
+        }
+        let end=str.indexOf(')')
+        if (end<0||end<start) {
+            break;
+        }
+        result.push(str.substring(start+1,end))
+        str=str.substring(end+1)
+    }
+    console.log(result.join(', '))
+}
