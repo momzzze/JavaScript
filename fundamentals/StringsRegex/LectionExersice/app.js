a410280a62087c62bad6302391e26324ecc68ec0
momzzze <momzzze@abv.bg>
2018-02-03 00:48:45 +0200

    С

--- fundamentals/StringsRegex/LectionExersice/app.js
+++ fundamentals/StringsRegex/LectionExersice/app.js
@@ -32,7 +32,8 @@
 //}
 
 
-function extractText(str) {
+/*function extractText(str) {
+
     let result=[]
     
     while (true) {
@@ -48,4 +49,38 @@ function extractText(str) {
         str=str.substring(end+1)
     }
     console.log(result.join(', '))
+}*/
+
+/*function name(arr) {
+    let towns=[]
+    let sum=0
+    for (let index = 0; index < arr.length; index++) {
+       let tokens= arr[index].split('|').filter(a => a !=='')
+        towns.push(tokens[0].trim())
+        sum+=Number(tokens[1].trim())
+    }
+    console.log(towns.join(', '))
+    console.log(sum)
+}*/
+
+
+/*function name(arr) {
+    let products=arr.filter((str,i)=>i%2===0).join(', ')
+    let sum=arr.filter((str,i)=>i%2!==0).map(str=>Number(str)).reduce((a,b)=>a+b)
+    console.log(`You purchased ${products} for a total sum of ${sum}`)
+}*/
+
+function name(arr) {
+    let finalResult=[]
+    for (let i = 0; i < arr.length; i++) {
+        let token = arr[i].split('@')
+        let domains=token[1].split('.')
+        let result=token[0]+'.'
+       for (let str of domains) {
+         result+=str[0] 
+       }
+        finalResult.push(result)       
+        
+    }console.log(finalResult.join(', '))
 }
+name(['peshoo@gmail.com', 'todor_43@mail.dir.bg','foo@bar.com'])
