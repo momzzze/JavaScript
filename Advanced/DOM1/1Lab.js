67d30c9da58c5559a30547663fc8a55d3796e24f
momzzze <momzzze@abv.bg>
2018-02-14 20:11:28 +0200

    a

--- Advanced/DOM1/1Lab.js
+++ Advanced/DOM1/1Lab.js
@@ -16,3 +16,10 @@
             document.getElementById("result").textContent+=item.textContent+'\n'
         }
     }
+    
+    function colorize() {
+        let arr=document.getElementsByTagName('table')[0].children[0].children;
+        for (let i = 1; i < arr.length; i+=2) {
+            arr[i].style.backgroundColor='Teal';
+        }
+    }
