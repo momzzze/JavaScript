75639ed503adf1a56c0ce94f0390ab8b706a4a48
momzzze <momzzze@abv.bg>
2018-06-20 20:40:26 +0300

    s

---
+++ JSWeb/React/project/app/app.js
@@ -0,0 +1,14 @@
+let React=require('react')
+let ReactDOM=require('react-dom')
+
+const App=React.createClass({
+  render:function(){
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(<App />,document.getElementById("main"))
