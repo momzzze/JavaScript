38e8ec4e29ccdf95c8ca6bf05feb07e1083a76b9
momzzze <momzzze@abv.bg>
2020-08-14 10:09:26 +0300

    s
    s

--- JSWeb/React/CRD2020/basics/my-app/src/App.js
+++ JSWeb/React/CRD2020/basics/my-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -11,12 +12,18 @@ class App extends Component {
       monsters: [],
       searchField: ''
     }
+   // this.handleChange = this.handleChange.bind(this)
 
   }
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => res.json()).then(users => this.setState({ monsters: users }))
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
     //const { monsters, searchField } = this.state;
     //const filteredMonsters = monsters.filter(m => m.name.toLowerCase().includes(searchField.toLowerCase))
@@ -25,9 +32,12 @@ class App extends Component {
     const filteredMonsters = monsters.filter(m => m.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
-        <input onChange={e => {
-          this.setState({ searchField: e.target.value }, () => console.log(this.state))
-        }} type="text" placeholder="search monsters" />
+
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+
         <CardList monsters={filteredMonsters} />
       </div>
     )
